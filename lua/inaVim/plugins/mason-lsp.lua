57695c279560562bf2da13f774089d972ee91729
Muntyq <aleix.muntal78@gmail.com>
2026-05-03 06:55:57 +0200

    added lsp

---
+++ lua/inaVim/plugins/mason-lsp.lua
@@ -0,0 +1,60 @@
+-- configs for specific lsp server
+--
+vim.lsp.config('lua_ls', {
+	settings = { Lua = { diagnostics = { globals = { 'vim' }}}}
+})
+vim.lsp.config('qmlls', {
+	-- if qml doesn't work change this to where 'pacman -Ql qt6-declarative | grep qmlls' points
+	cmd = { '/usr/lib/qt6/bin/qmlls'}
+})
+
+-- pluguin for handling the lsp
+--
+return {
+	{
+		'mason-org/mason.nvim',
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗"
+				}
+			}
+		}
+	},
+	{
+		'neovim/nvim-lspconfig',
+		config = function()
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(args)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = args.buf, desc = "LSP: " .. desc })
+
+					end
+
+					map('grr', vim.lsp.buf.rename, '[R]ename')
+					map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction')
+					map('grh', vim.lsp.buf.hover, '[H]over Documentation')
+					map('grd', vim.lsp.buf.type_definition, '[G]oto [D]efinition')
+					map('grn', function() vim.diagnostic.jump({ count = 1 }) end, '[G]oto [N]ext Diagnostic')
+					map('grp', function() vim.diagnostic.jump({ count = -1 }) end, '[G]oto [P]revious Diagnostic')
+
+				end
+			})
+		end
+
+	},
+	{
+		'mason-org/mason-lspconfig.nvim',
+		opts = {
+			-- use the vim.lsp.config names! source: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+			ensure_installed = { "lua_ls", "gopls" }
+		},
+		dependencies = {
+			'mason-org/mason.nvim',
+			'neovim/nvim-lspconfig',
+		},
+	}
+}
--- lua/inaVim/plugins/tree-sitter.lua
+++ lua/inaVim/plugins/tree-sitter.lua
@@ -2,6 +2,33 @@
 --
 return {
 	{
-		'tree-sitter/tree-sitter',
+		'nvim-treesitter/nvim-treesitter',
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local langs = { 'go', 'gomod', 'gosum', 'lua', 'html', 'bash' } 
+			require('nvim-treesitter').install(langs)
+			vim.api.nvim_create_autocmd('FileType', {
+				callback = function(args)
+					local buf, filetype = args.buf, args.match
+
+					local language = vim.treesitter.language.get_lang(filetype)
+					if not language then return end
+
+					--Check if parser exists and load it
+					if not vim.treesitter.language.add(language) then return end
+
+					--Enable syntax highlighting and other features
+					vim.treesitter.start(buf, language)
+
+					--Folds
+					--vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+					--vim.wo[0][0].foldmethod = 'expr'
+
+					--Indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
 	}
 }
